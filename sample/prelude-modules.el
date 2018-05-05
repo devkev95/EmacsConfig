dd688009dd1b2b619ea9e8bd9426a9528a243582
Kevin <devkev95@gmail.com>
2018-05-05 12:56:33 -0600

    First iteration

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -13,10 +13,10 @@
 
 ;;; Programming languages support
 (require 'prelude-c)
-;; (require 'prelude-clojure)
+(require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
-;; (require 'prelude-css)
+(require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
@@ -34,7 +34,7 @@
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
-;; (require 'prelude-scss)
-;; (require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-scss)
+(require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-;; (require 'prelude-yaml)
+(require 'prelude-yaml)
